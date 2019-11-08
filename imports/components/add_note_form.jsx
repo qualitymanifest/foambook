0575c82df9293a0f7672351a41f996b49cd66528
cdr <qualitymanifest@gmail.com>
2019-11-08 16:52:01 -0500

    Make AddNoteForm a functional component

--- imports/components/add_note_form.jsx
+++ imports/components/add_note_form.jsx
@@ -1,5 +1,5 @@
 import { Meteor } from "meteor/meteor";
-import React, { Component } from "react";
+import React from "react";
 import { Form } from "informed";
 import { debounce } from "lodash";
 import { withTracker } from "meteor/react-meteor-data";
@@ -16,123 +16,113 @@ import checkUserStatus from "../utils/checkUserStatus";
 Moment.tz.setDefault("Etc/UTC");
 let apiHandle;
 
-class AddNoteForm extends Component {
-	onSubmit(values) {
-		noteSubmitMethod(values, apiHandle);
+const AddNoteForm = ({ notes, user = "LOADING" }) => {
+
+	const checkUser = checkUserStatus(user, "add_note_form");
+	if (!checkUser.shouldRender) {
+		return checkUser.renderInstead;
 	}
 
-	transferApi(formApi) {
+	const { railroad, city, state, timezone } = user.preferences;
+
+	const defaultValues = {
+		railroad,
+		city,
+		state,
+		dateTime: timezone ? Moment().tz(timezone).format("MM-DD-YY HH:mm") : ""
+	};
+
+	const onSubmit = values => noteSubmitMethod(values, apiHandle);
+
+	const transferApi = formApi => {
 		apiHandle = formApi;
 	}
 
-	render() {
-		const { notes, user } = this.props;
-		const checkUser = checkUserStatus(user, "add_note_form");
-		if (!checkUser.shouldRender) {
-			return checkUser.renderInstead;
-		}
+	return (
+		<div className="text-center fadeIn">
+			<h3>Submit a note</h3>
+			<Form
+				className="form-group"
+				getApi={transferApi}
+				onSubmit={debounce(onSubmit, 200)}
+				initialValues={defaultValues}
+			>
+				{({ formApi, formState }) => (
+					<>
 
-		let defaultValues = null;
-		if (user.preferences) {
-			// user is logged in, and has preferences. create defaults object for form!
-			const { railroad, city, state, timezone } = user.preferences;
-			defaultValues = {
-				railroad,
-				city,
-				state,
-				dateTime: timezone ? Moment().tz(timezone).format("MM-DD-YY HH:mm") : ""
-			};
-		}
-		return (
-			<div className="text-center fadeIn">
-				<h3>Submit a note</h3>
-				<Form
-					className="form-group"
-					getApi={this.transferApi}
-					onSubmit={debounce(this.onSubmit.bind(this), 200)}
-					initialValues={defaultValues}
-				>
-					{({ formApi, formState }) => (
-						<>
+						<label>Railroad</label>
+						<FieldWithError
+							className="form-control"
+							field="railroad"
+							placeholder="E.G. CSX"
+							validateOnBlur
+							validate={validSubRailroad}
+							notify={["symbol"]}
+						/>
 
-							<label>Railroad</label>
-							<FieldWithError
-								className="form-control"
-								field="railroad"
-								placeholder="E.G. CSX"
-								validateOnBlur
-								validate={validSubRailroad}
-								notify={["symbol"]}
-							/>
-
-							<div id="locationFieldSpan">
-								<div id="cityField">
-									<label>Location</label>
-									<FieldWithError
-										className="form-control"
-										field="city"
-										maxLength="30"
-										placeholder="e.g. PITTSBURGH"
-										validateOnBlur
-										validate={validSubCity}
-									/>
-								</div>
-								<div id="stateField">
-									<label>State</label>
-									<FieldWithError
-										className="form-control"
-										field="state"
-										maxLength="2"
-										placeholder="E.G. PA"
-										validateOnBlur
-										validate={validSubState}
-									/>
-								</div>
+						<div id="locationFieldSpan">
+							<div id="cityField">
+								<label>Location</label>
+								<FieldWithError
+									className="form-control"
+									field="city"
+									maxLength="30"
+									placeholder="e.g. PITTSBURGH"
+									validateOnBlur
+									validate={validSubCity}
+								/>
 							</div>
+							<div id="stateField">
+								<label>State</label>
+								<FieldWithError
+									className="form-control"
+									field="state"
+									maxLength="2"
+									placeholder="E.G. PA"
+									validateOnBlur
+									validate={validSubState}
+								/>
+							</div>
+						</div>
 
-							<label>Symbol</label>
-							<FieldWithError
-								className="form-control"
-								field="symbol"
-								id="symbol"
-								placeholder="E.G. Q138"
-								validateOnBlur
-								validate={validSubSymbol}
-								autoFocus
-							/>
+						<label>Symbol</label>
+						<FieldWithError
+							className="form-control"
+							field="symbol"
+							id="symbol"
+							placeholder="E.G. Q138"
+							validateOnBlur
+							validate={validSubSymbol}
+							autoFocus
+						/>
 
-							<label>Date/Time</label>
-							<DateTime
-								className="form-control"
-								field="dateTime"
-								placeholder="MM-DD-YY 23:59"
-								validateOnBlur
-								validate={validSubDateTime} />
+						<label>Date/Time</label>
+						<DateTime
+							className="form-control"
+							field="dateTime"
+							placeholder="MM-DD-YY 23:59"
+							validateOnBlur
+							validate={validSubDateTime} />
 
-							<button type="submit" className="btn btn-primary">
-								Submit
+						<button type="submit" className="btn btn-primary">
+							Submit
 							</button>
 
-						</>
-					)}
-				</Form>
-				<NotesTable
-					notes={notes}
-					user={user}
-					deleteFunc={notesDeleteMethod}
-					appLocation="add_note_form"
-					caption="Recent Submissions - All Users"
-				/>
-			</div>
-		);
-	}
+					</>
+				)}
+			</Form>
+			<NotesTable
+				notes={notes}
+				user={user}
+				deleteFunc={notesDeleteMethod}
+				appLocation="add_note_form"
+				caption="Recent Submissions - All Users"
+			/>
+		</div>
+	);
 }
 
-AddNoteForm.defaultProps = {
-	user: "LOADING"
-};
-
-const MeteorAddNoteForm = withTracker(() => {
+export default withTracker(() => {
 	Meteor.subscribe("notes", 5);
 	return {
 		notes: Notes.findFromPublication("notes", {}, {
@@ -142,5 +132,3 @@ const MeteorAddNoteForm = withTracker(() => {
 		user: Meteor.user()
 	};
 })(AddNoteForm);
-
-export default MeteorAddNoteForm;
