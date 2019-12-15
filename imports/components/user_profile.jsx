dec884fc233fb61f6b78189bc9f16f56f61f4ab6
cdr <qualitymanifest@gmail.com>
2019-12-15 03:33:16 -0500

    Drop redux: Handle query incrementing on UserProfile

--- imports/components/user_profile.jsx
+++ imports/components/user_profile.jsx
@@ -1,18 +1,18 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import { withTracker } from "meteor/react-meteor-data";
-import { connect } from "react-redux";
 import { debounce } from "lodash";
 
 import { downloadMethod } from "../methods";
 import PreferenceForm from "./preference_form";
 import { Notes } from "../collections/notes";
 import NotesTable from "./notes_table";
-import { incrementPages } from "../../client/actions";
 import checkUserStatus from "../utils/checkUserStatus";
 import { FORM_DEBOUNCE_MS } from "../utils/constants";
 
-const UserProfile = ({ user = "LOADING", notes, paginate }) => {
+const loadNum = new ReactiveVar(5);
+
+const UserProfile = ({ user = "LOADING", notes }) => {
 	const checkUser = checkUserStatus(user, "user_profile");
 	if (!checkUser.shouldRender) {
 		return checkUser.renderInstead;
@@ -31,7 +31,7 @@ const UserProfile = ({ user = "LOADING", notes, paginate }) => {
 				caption="Your Recent Submissions" />
 			{!!notes.length &&
 				<>
-					<button className="btn btn-primary" onClick={paginate} >Load More</button>
+					<button className="btn btn-primary" onClick={() => loadNum.set(loadNum.get() + 5)} >Load More</button>
 					<br /><br />
 					<button className="btn btn-default" onClick={debounce(downloadMethod, FORM_DEBOUNCE_MS)}>
 						Download Notes
@@ -42,25 +42,15 @@ const UserProfile = ({ user = "LOADING", notes, paginate }) => {
 	);
 };
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		paginate: () => {
-			dispatch(incrementPages());
-		}
-	};
-};
-
-const MeteorUserProfile = withTracker(({ profileState }) => {
-	Meteor.subscribe("notes.user", profileState.loadNum);
+export default withTracker(() => {
+	Meteor.subscribe("notes.user", loadNum.get());
 	return {
 		// specifying userId again doesn't seem necessary but can prevent future bugs
 		// see "Always use specific queries to fetch data" in guide
 		user: Meteor.user(),
 		notes: Notes.findFromPublication("notes.user", { userId: Meteor.userId() }, {
 			sort: { createdAt: -1 },
-			limit: profileState.loadNum
+			limit: loadNum.get()
 		}).fetch()
 	};
 })(UserProfile);
-
-export default connect(({ profileState }) => ({ profileState }), mapDispatchToProps)(MeteorUserProfile);
